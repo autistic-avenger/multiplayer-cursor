6556ab7a48e714dd683d0c61986eecb3b5893297
Aditya Kumar <aditya4u164@gmail.com>
2025-11-14 02:49:45 +0530

    Added connect disconnect obj logic

--- Server/server.js
+++ Server/server.js
@@ -4,19 +4,33 @@ import http from 'http'
 
 const Port = 8080
 const app = express()
-const server = http.createServer(app,{
-    cors:{
-        origin:"*"
-    }
-})
-const io = new Server(server)
+const server = http.createServer(app)
+
+const io = new Server(server,{
+  cors: {
+    origin: "*",   
+    methods: ["GET", "POST"]
+  }})
+let users = {}
 
 app.get('/',(req,res)=>{
     res.send("Based Sigma!👌")
 })
+io.on("connection",(socket)=>{
+    console.log(socket.id ,"Connected 👾" );
+    users[socket.id] = [undefined,undefined]
+
+    socket.on("disconnect",()=>{
+        delete users[socket.id];
+        console.log(socket.id ,"Disconnected 👾");
+        
+    })
+        
+})
+
 
 
 server.listen(Port,()=>{
-    console.log(`Server is Up and Listening to:\nhttps://localhost:${Port}`)
+    console.log(`Server is Up and Listening to:\nhttps://localhost:${Port}\n\n`)
 })
 
